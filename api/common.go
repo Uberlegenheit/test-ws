a088714bd586587b4d880dbd9b3c265f96da92a2
Ungerechtigkeit <l.militech.l@gmail.com>
2024-06-10 15:16:58 +0300

    upd (1)

--- api/common.go
+++ api/common.go
@@ -1,12 +1,27 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net"
 	"net/http"
 )
 
+func GetLocalIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP
+}
+
 func (api *API) Index(c *gin.Context) {
-	c.String(http.StatusOK, "This is a service 'nuhai bebru'")
+	c.String(http.StatusOK, fmt.Sprintf("This is a service 'nuhai bebru', IP: (%v)", GetLocalIP()))
 }
 
 func (api *API) Health(c *gin.Context) {
